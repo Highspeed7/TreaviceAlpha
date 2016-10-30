2ff681a330f8761064913eaa452efa694d281fe4
Highspeed7 <b.sanders7777@edmail.edcc.edu>
2016-10-29 18:22:44 -0700

    Added AppComponent Added NavComponent Added overwriting styles for site

---
+++ TreaviceAlpha/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+    selector: "trvc-app",
+    template: `
+    <navbar></navbar>
+    <h1>Welcome to Treavice</h1>`
+})
+
+export class AppComponent { }
---
+++ TreaviceAlpha/app/app.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule }      from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./nav/navbar.component";
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [
+        AppComponent,
+        NavbarComponent
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
