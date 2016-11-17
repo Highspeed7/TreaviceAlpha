61dd40c56ff7df4599bec76c38156fa3b4a1cca7
Brian <b.sanders7777@edmail.edcc.edu>
2016-11-16 18:40:07 -0800

    deleting all js and js.map files to exclusion within source.

--- TreaviceAlpha/app/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var RegisterFormModel = (function () {
-    function RegisterFormModel() {
-    }
-    return RegisterFormModel;
-}());
-exports.RegisterFormModel = RegisterFormModel;
-//# sourceMappingURL=index.js.map
