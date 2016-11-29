6764f01e7734273cffd3bd778b3d7d4375b61805
Brian <b.sanders7777@edmail.edcc.edu>
2016-11-28 23:03:53 -0800

    Added profile model.

---
+++ TreaviceAlpha/Models/Profile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TreaviceAlpha.Models
+{
+    public class Profile
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
