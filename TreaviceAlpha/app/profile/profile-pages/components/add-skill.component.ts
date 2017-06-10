31aa19c4bb43b883593abfd40a2cfdb592efa3a7
Brian West <bwest1986@outlook.com>
2017-06-09 18:26:27 -0700

    Beginning to flesh out the services section of the profile.

--- TreaviceAlpha/app/profile/profile-pages/components/add-skill.component.ts
+++ TreaviceAlpha/app/profile/profile-pages/components/add-skill.component.ts
@@ -1,14 +1,36 @@
-﻿import { Component } from "@angular/core";
+﻿import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: "add-skill-form",
     templateUrl: "app/profile/profile-pages/components/add-skill-form.component.html"
 })
 
-export class AddSkillComponent {
+export class AddSkillComponent implements OnInit {
+    public skillForm: FormGroup;
+
+    constructor(private fb: FormBuilder){}
+
     public categories = [
         { name: "Cat 1" },
         { name: "Cat 2" },
         { name: "Cat 3" }
     ];
+
+    public ngOnInit() {
+        this.skillForm = this.fb.group({
+            title: this.initSkillTitle(),
+            desc: this.initSkillDesc(),
+            isNeg: ["no"],
+            category: ["", Validators.required]
+        });
+    }
+
+    private initSkillTitle() {
+        return ["", Validators.required];
+    }
+
+    private initSkillDesc() {
+        return ["", Validators.required];
+    }
 }
