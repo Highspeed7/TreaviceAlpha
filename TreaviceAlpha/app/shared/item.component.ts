78cd6d5ad25ff181616fc5131a8626e92ed7ff8b
Brian West <bwest1986@outlook.com>
2017-06-19 01:22:41 -0700

    Trove item design changes.

--- TreaviceAlpha/app/shared/item.component.ts
+++ TreaviceAlpha/app/shared/item.component.ts
@@ -1,8 +1,27 @@
-﻿import { Component } from "@angular/core";
+﻿import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "item",
-    templateUrl: "app/shared/item.component.html"
+    templateUrl: "app/shared/item.component.html",
+    styles: [`
+        :host {
+            cursor: pointer;
+        }
+        
+        div.item {
+            min-height: 100px;
+            max-height: 100px;
+            padding: 5px;
+            border: 1px solid grey;
+            border-collapse: collapse;
+        }
+    `]
 })
 
-export class ItemComponent {}
+export class ItemComponent {
+    @Input()
+    public title: string;
+
+    @Input()
+    public points: number;
+}
