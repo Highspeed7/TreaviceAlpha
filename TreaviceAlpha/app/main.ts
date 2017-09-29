4164928211bf664512aaa5f1db0012c9ba04e2f2
Brian West <therealhighspeed7@gmail.com>
2017-09-29 01:33:45 -0700

    Updated angular Made less work

--- TreaviceAlpha/app/main.ts
+++ TreaviceAlpha/app/main.ts
@@ -1,10 +1,9 @@
 ﻿///<reference path="./../node_modules/@types/core-js/index.d.ts"/>
-import "../Content/site.less";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
+import "../Content/site.less";
 import { Http } from "@angular/http";
 
 import { AppModule } from "./app.module";
 
 const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule, [Http]);
+platform.bootstrapModule(AppModule);
--- TreaviceAlpha/webpack.config.js
+++ TreaviceAlpha/webpack.config.js
@@ -1,4 +1,5 @@
-﻿require('es6-promise').polyfill();
+﻿const helpers = require("./helpers");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     // define entry point
     entry: {
@@ -8,28 +9,33 @@ module.exports = {
     },
     // define output point
     output: {
-        path: "dist",
-        filename: "js/[name].js"
+        path: helpers.root("TreaviceAlpha/dist"),
+        filename: "js/[name].js",
+        sourceMapFilename: "[file].map"
     },
 
     module: {
         rules: [
-            {
-                use: "tslint-loader",
-                test: /\.ts$/
-            },
+            //{
+            //    use: "tslint-loader",
+            //    test: /\.ts$/
+            //},
             {
                 use: "awesome-typescript-loader",
                 test: /\.ts$/
             },
             {
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "less-loader"
-                ],
-                test: "/\.less$/"
+                test: /\.less$/,
+                exclude: helpers.root("app"),
+                loader: ExtractTextPlugin.extract({
+                    fallback: ["style-loader"],
+                    use: ["css-loader", "less-loader"]
+                })
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("styles/styles.css")
+    ]
+
 };
